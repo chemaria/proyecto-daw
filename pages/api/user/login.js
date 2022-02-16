b85034bc5faba7a1bbae80a65b20f378278119b9
chema <josecruzigle@gmail.com>
2022-02-16 20:16:50 +0100

    cambios api login

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -8,24 +8,24 @@ export default async function handler(req, res) {
 
   // comprobamos si user esta bd
   const userDb = await mySqlDbConnect({
-    query: 'SELECT id FROM users WHERE `username` = ?',
+    query: 'SELECT id, password FROM users WHERE `username` = ?',
     params: [dataLogin.user],
   })
+  console.log(userDb)
   // verificamos si user o password estan vacios y si user existe en bbdd
-  if (!(dataLogin.user || dataLogin.password) || userDb.length === 0) {
+  if (!(dataLogin.user && dataLogin.password) && userDb.length === 0) {
     return res.status(401).json({ error: 'error de autenticación' })
   }
-
-  const reqData = await mySqlDbConnect({
-    query: 'SELECT password FROM users WHERE `username` = ?',
-    params: [dataLogin.user],
-  })
   // const pass = await bcrypt.hash(dataLogin.password, 10)
-  const match = await bcrypt.compare(dataLogin.password, reqData[0].password)
+  const match = await bcrypt.compare(dataLogin.password, userDb[0].password)
 
   if (match) {
     res.status(201).send('perfe!')
+    const generateToken = {
+      id: userDb[0],
+      username: dataLogin.user,
+    }
   } else {
-    res.status(401).json({ error: 'error de autenticación' })
+    res.status(401).json({ error: 'error de autenticación 2' })
   }
 }
