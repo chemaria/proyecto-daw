8bef13546a8e61bed685769c61f12c23646d947d
chema <josecruzigle@gmail.com>
2022-02-18 21:56:30 +0100

    cambios middleawre

--- pages/admin/_middleware.js
+++ pages/admin/_middleware.js
@@ -1,3 +1,10 @@
-export default async function middleware(req, res) {
-  console.log(req.cookies.jwt)
+import Jwt from 'jsonwebtoken'
+export default function middleware(req, res) {
+  if (!req.cookies.jwt) {
+    return res.status(401).send('adios')
+  }
+  const verifyToken = Jwt.verify(req.cookies.jwt, process.env.JWT_SECRET_KEY)
+  if (!verifyToken) {
+    return res.status(401).send('adios')
+  }
 }
