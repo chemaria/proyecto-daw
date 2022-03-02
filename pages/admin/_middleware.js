2046b466e78bb9a94f4ea3b4fd38354b3f426c5c
unknown <josecruzigle@gmail.com>
2022-03-02 09:42:05 +0100

    form state

--- pages/admin/_middleware.js
+++ pages/admin/_middleware.js
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import jwt from 'jsonwebtoken'
 
 export default async function middleware(req, res) {
+  const response = new NextResponse()
   if (!req.cookies.jwt) {
     return new NextResponse('Auth Required', {
       status: 401,
@@ -10,9 +11,9 @@ export default async function middleware(req, res) {
   try {
     await jwt.verify(req.cookies.jwt, process.env.JWT_SECRET_KEY)
   } catch (error) {
-    // borrar cookies jwt
-    return new NextResponse('Auth Required', {
-      status: 401,
-    })
+    const res = NextResponse.redirect('/login')
+    console.log('estas pasando por aqui?')
+    res.clearCookie('jwt')
+    return res
   }
 }
